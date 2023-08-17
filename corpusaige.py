7143c14079ffebb7da0deb53aa509dec8f50ac7a
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2023-08-17 23:14:04 +0200

    Added pyinstaller as dependency

---
+++ corpusaige.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Corpusaige is a Python tool (and utility library) enabling AI-powered systems analysis 
+through deep exploration and understanding of comprehensive document sets and source code.
+@copyright: Copyright © 2023 Iwan van der Kleijn
+@license: MIT
+"""
+
+from corpusaige.ui.cli import main
+
+if __name__ == "__main__":
+    main()
